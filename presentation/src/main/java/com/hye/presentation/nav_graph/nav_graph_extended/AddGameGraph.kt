ae0e403bf1216f833ebdef57a9b6045d6607af26
hyewoon <likethebegin@naver.com>
2025-10-12 23:08:41 +0900

    chore: hiltViewModel 제거

--- presentation/src/main/java/com/hye/presentation/nav_graph/nav_graph_extended/AddGameGraph.kt
+++ presentation/src/main/java/com/hye/presentation/nav_graph/nav_graph_extended/AddGameGraph.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import com.hye.presentation.nav_graph.ScreenRoutDef
+import com.hye.presentation.ui.model.BookmarkViewModel
 import com.hye.presentation.ui.screen.game.DrawScreen
 import com.hye.presentation.ui.screen.game.SearchScreen
 import com.hye.presentation.ui.screen.game.SpeechToTextScreen
@@ -14,7 +15,6 @@ import com.hye.presentation.ui.model.GameViewModel
 import com.hye.presentation.ui.model.SharedViewModel
 
 fun NavGraphBuilder.addGameGraph(
-    gameViewModel: GameViewModel,
     sharedViewModel: SharedViewModel,
     onNavigateToDrawScreen: () -> Unit,
     onNavigateToSearchScreen: () -> Unit,
@@ -28,36 +28,31 @@ fun NavGraphBuilder.addGameGraph(
         composable<ScreenRoutDef.GameFlow.DrawScreen> {
             DrawScreen(
                 onNavigateToDrawScreen = onNavigateToDrawScreen,
-                gameViewModel = gameViewModel,
                 sharedViewModel = sharedViewModel,
             )
         }
         composable<ScreenRoutDef.GameFlow.SearchScreen> {
             SearchScreen(
                 onNavigateToSearchScreen = onNavigateToSearchScreen,
-                gameViewModel = gameViewModel,
                 sharedViewModel = sharedViewModel
             )
         }
         composable<ScreenRoutDef.GameFlow.VocabularyScreen> {
             VocabularyScreen(
                 onNavigateToVocabularyScreen = onNavigateToVocabularyScreen,
-                gameViewModel = gameViewModel,
-                sharedViewModel = sharedViewModel,
+                sharedViewModel = sharedViewModel
             )
 
         }
         composable<ScreenRoutDef.GameFlow.TextToSpeechScreen> {
             TextToSpeechScreen(
                 onNavigateToTextToSpeechScreen = onNavigateToTextToSpeechScreen,
-                gameViewModel = gameViewModel,
                 sharedViewModel = sharedViewModel
             )
         }
         composable<ScreenRoutDef.GameFlow.SpeechToTextScreen> {
             SpeechToTextScreen(
                 onNavigateToSpeechToTextScreen = onNavigateToSpeechToTextScreen,
-                gameViewModel = gameViewModel,
                 sharedViewModel = sharedViewModel
             )
 
