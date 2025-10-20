d4f2fd94548467a86a87d1a6e9630e9c7f1f65d5
hyewoon <likethebegin@naver.com>
2025-10-21 04:19:46 +0900

    chore: 사용하지 않은 클래스 및 screen삭제

--- presentation/src/main/java/com/hye/presentation/model/Bookmark.kt
+++
@@ -1,9 +0,0 @@
-package com.hye.presentation.model
-
-import com.hye.presentation.R
-
-data class Bookmark(
-    val korean: String = "",
-    val english: String ="",
-    val star: Int = R.drawable.star
-)
